96e3a1cf0fa477c6833638c2d5b2c712c7628cb8
mmarquis <mmarquis89@gmail.com>
2019-11-11 16:47:01 -0500

    Switch align LED to start of trial only (instead of start and end)

--- Panels_code/run_panels_trial.m
+++ Panels_code/run_panels_trial.m
@@ -89,9 +89,8 @@ end%if
 
 % Alignment IR LED command 
 alignLEDCommand = zeroStim;
-LEDOnSamples = mD.SAMPLING_RATE * 0.1; % LED on for first and last 100 ms of trial
+LEDOnSamples = mD.SAMPLING_RATE * 0.1; % LED on for first 100 ms of each trial
 alignLEDCommand(1:LEDOnSamples) = 1;
-alignLEDCommand(end-LEDOnSamples:end) = 1;
 
 % % Panels
 panelsStartTrigger = siTrigger; % sending the same trigger command to scanimage and the panels
